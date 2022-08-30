7533967e383d63b889e70ee2aae359da510cf104
DaweUrbi <urban.da@icloud.com>
2022-08-30 13:00:26 -0700

    my fridge - autocomplete,list

--- src/Components/MyFridge.jsx
+++ src/Components/MyFridge.jsx
@@ -1,46 +1,107 @@
-import React from 'react'
-import axios from 'axios'
-import { useState } from 'react'
-import { useEffect } from 'react'
-
-
-
-
-
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function MyFridge() {
+	const [query, setQuery] = useState("");
+	const [autocomplete, setAutocomplete] = useState([]);
+	const [ingredient, setIngredient] = useState("");
+	const [ingredientId, setIngredientId] = useState("");
+	const [ingredientImage, setIngredientImage] = useState("");
+	const [fridgeList, setFridgeList] = useState([]);
 
-    const [query, setQuery] = useState("")
+	useEffect(() => {
+		const loadIngredients = async () => {
+			const response = await axios.get(
+				`https://api.spoonacular.com/food/ingredients/autocomplete?apiKey=88749994321f4e4eaa03a853e6edf42c&query=${query}&metaInformation=true`
+			);
+			setAutocomplete(response.data);
+		};
+		loadIngredients();
+	}, [query]);
 
-    const [fridge, setFridge] = useState([]);  
-    useEffect(() => {
-        const loadIngredients = async () => {
-            const response = await axios.get(`https://api.spoonacular.com/food/ingredients/autocomplete?apiKey=88749994321f4e4eaa03a853e6edf42c&query=${query}&metaInformation=true`);
-            console.log(response.data);
-            setFridge(response.data);
-        }
-        loadIngredients();
-    }, [query])
+	const handleOnChange = (e) => {
+		// e.preventDefault()
+		setQuery(e.target.value);
+		document.getElementById("whisper").style.display = "block";
+	};
 
-    const handleOnChange = (e) => {
-        e.preventDefault()
-        setQuery(e.target.value)
-      }
+	const temporaryList = (id, name, image) => {
+		setIngredientId(id);
+		setIngredient(name);
+		setIngredientImage(image);
+		document.getElementById("searchInput").value = name;
+		document.getElementById("whisper").style.display = "none";
+	};
 
-  return (
-    <div>
-        <h2>My Fridge</h2>
-        <form action="">
-            <input type="text" list="pol" placeholder="Search" onChange={(e) => handleOnChange(e)} />
-            <datalist id="pol">
-            {fridge.map((item, i) => <option>{ item.name }</option>)} 
-            </datalist>
-            <input type="submit" value="Add" />
-        </form>
-        <div>
-        <ul>  </ul>
-        <button>X</button>
-        </div>
-    </div>
-  )
+	const sendToFridgeList = () => {
+		if (ingredientId != "") {
+			setFridgeList((oldArray) => [
+				...oldArray,
+				{ id: ingredientId, name: ingredient, image: ingredientImage },
+			]);
+			setIngredient("");
+			setIngredientId("");
+			setIngredientImage("");
+			document.getElementById("searchInput").value = "";
+		}
+	};
+
+	const deleteFromFridgeList = (id) => {
+		setFridgeList((oldArray) => oldArray.filter((item) => item.id !== id));
+	};
+
+	const imgStyle = {
+		width: "3%",
+		height: "3%",
+	};
+
+	const listStyle = {
+		display: "flex",
+		flexDirection: "column",
+	};
+
+	return (
+		<div>
+			<h2>My Fridge</h2>
+			<form>
+				<input
+					type="text"
+					id="searchInput"
+					list="pol"
+					autoComplete="off"
+					placeholder="Search"
+					onChange={(e) => handleOnChange(e)}
+				/>
+				<input type="button" value="Add" onClick={() => sendToFridgeList()} />
+				<ul id="whisper">
+					{autocomplete.map((item, i) => (
+						<div
+							onClick={() => temporaryList(item.id, item.name, item.image)}
+							key={item.id}
+						>
+							{item.name}
+						</div>
+					))}
+				</ul>
+			</form>
+			<div style={listStyle}>
+				{fridgeList.map((item, i) => (
+					<div key={item.id}>
+						<img
+							style={imgStyle}
+							src={
+								"https://spoonacular.com/cdn/ingredients_100x100/" + item.image
+							}
+						/>
+						<span> {item.name} </span>
+						<button onClick={() => deleteFromFridgeList(item.id)}>
+							{" "}
+							Remove{" "}
+						</button>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
