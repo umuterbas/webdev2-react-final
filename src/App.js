9355bf7946db1ac417b4e200c68cc85138c1e119
Guilherme Maciel de Almeida <guilherme@gmaciel.com>
2022-09-01 13:38:21 -0700

    Fix Components folder name

--- src/App.js
+++ src/App.js
@@ -5,9 +5,18 @@ import Recipespage from "./Components/RecipeCard/MidSection/Recipespage";
 import Test from "./Backend/test";
 import MyFridge from "./Components/MyFridgeList/MyFridge";
 
+import { AuthContextProvider } from './Components/nav/AuthContext';
+import Nav from "./Components/nav/Nav"
+import {  BrowserRouter as Router,   Routes,   Route} from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
+      <AuthContextProvider>
+        <Router>
+          <Nav />
+        </Router>
+      </AuthContextProvider>
       <Recipespage />
       {/* <Test/> */}
       {/* <MyFridge /> */}
