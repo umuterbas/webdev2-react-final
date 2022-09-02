00233ffb23cb7b34cdd0d401e256351912108415
DaweUrbi <urban.da@icloud.com>
2022-09-02 14:34:24 -0700

    hero page

---
+++ src/Components/HeroPage/HeroPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Button, HeroDiv, NavDiv } from "../styles/HeroPage.styles";
+
+
+
+export default function HeroPage() {
+	
+
+	return (
+        <HeroDiv>
+            <NavDiv> Nav bar </NavDiv>
+            <Button>Enter</Button>
+
+        </HeroDiv>
+
+	);
+}
+
---
+++ src/Components/styles/HeroPage.styles.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const HeroDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    background-image: url(images/food.jpg);
+    background-size: cover;
+    background-opacity: 0.5;
+    width: 100%;
+    height: 100vh;
+    background-opacity: 0.5;
+`;
+
+export const NavDiv = styled.div`
+    background-color: white;
+    width: 100%;
+    height: 70px;
+
+`;
+
+export const Button = styled.button`
+    border-radius: 4px;
+    font-size: 2rem;
+    border: 1px solid black;
+    padding: 5px 30px;
+    color: black; 
+    margin-top: 400px;
+    background-color: white;
+    &:hover {
+        cursor: pointer;
+        box-shadow: 0 5px 15px rgba(0,0,0,0.3);
+        transition: box-shadow 0.2s ease-in-out;
+        
+    }
+`;
